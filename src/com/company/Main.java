10ddc7b2720d3924b9f8914e51392be604b17da3
Zason Founder <founder.zason@gmail.com>
2018-01-20 12:20:32 +0200

    solution written

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,55 @@
 package com.company;
 
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        List<Integer> numberList = new ArrayList<>(List.of(45, 15, 47));
+
+        System.out.println(highestCombination(numberList));
+
     }
+
+    static final Comparator<Integer> NUMBER_ORDER;
+
+    static{
+
+        NUMBER_ORDER = new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+
+                int o1o2 = Integer.valueOf(o1 + "" + o2);
+                int o2o1 = Integer.valueOf(o2 + "" + o1);
+
+                if (o1o2 < o2o1) {
+
+                    return 1;
+
+                } else {
+
+                    return -1;
+                }
+            }
+        };
+    }
+
+    static String highestCombination(List<Integer> numberList) {
+
+        Collections.sort(numberList,NUMBER_ORDER);
+
+        String finalString = "";
+
+        for (int number: numberList) {
+
+            finalString = finalString + number;
+        }
+
+        return finalString;
+    }
+
+
+
 }
