4f53e9cd2b0039d1fe31a316a01ea7094f6a41b0
Zason Founder <founder.zason@gmail.com>
2018-01-20 15:44:27 +0200

    JUnit class created

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,9 +13,13 @@ public class Main {
 
     }
 
-    static final Comparator<Integer> NUMBER_ORDER = new Comparator<Integer>() {
+    private static final Comparator<Integer> NUMBER_ORDER = new Comparator<Integer>() {
                 @Override
                 public int compare(Integer o1, Integer o2) {
+
+                    //The 2 compared integers are concatenated in both possible ways
+                    // to find the higher concatenated number and
+                    // swap the integers to achieve that number
 
                     int o1o2 = Integer.valueOf(o1 + "" + o2);
                     int o2o1 = Integer.valueOf(o2 + "" + o1);
@@ -34,18 +38,32 @@ public class Main {
 
     static String highestCombination(List<Integer> numberList) {
 
-        Collections.sort(numberList,NUMBER_ORDER);
+        numberList.sort(NUMBER_ORDER); //Sorts the list according to the criteria used in the comparator
 
-        String finalString = "";
+        StringBuilder sb = new StringBuilder();
 
-        for (int number: numberList) {
+        for (int number: numberList) { //Integers concatenated
 
-            finalString = finalString + number;
+            sb.append(number);
         }
 
-        return finalString;
+        return sb.toString();
     }
 
+    /* EXPLANATION:
 
+    My first thought as a solution was to build an algorithm that tries all different combinations that could be created from the integers given,
+    but I rejected that idea really fast because that would be complicated (as the number of integers is not fixed)
+    and it could lead to thousands/millions of loops (number of loops needed = N!)
 
+    My second thought as a solution was to write numbers as follows: 423 = 4 * 100 + 2 * 10 + 3
+    and compare the corresponding values. But on the existing problem this doesn't help because not all integers have the same number of digits.
+    So again the algorithm would have to be really complicated and hard to build.
+
+    My third thought (and my submitted solution) was to implement the basic concept of sorting a list, which is to compare the neighbouring values and
+    swap them, if needed. After thinking about it, I came to the conclusion that the following statement should be true in any case:
+
+    (AB < BA) <=> (ACB<BCA). So, this is the final solution I used.
+
+    */
 }
--- src/com/company/MainTest.java
+++ src/com/company/MainTest.java
@@ -12,9 +12,9 @@ class MainTest {
 
     @org.junit.jupiter.api.Test
     void highestCombination() {
-        List<Integer> numberList = new ArrayList<>(List.of(50,2, 1, 9));
+        List<Integer> numberList = new ArrayList<>(List.of(420,42, 423));
 
-        assertEquals("95021", Main.highestCombination(numberList));
+        assertEquals("42423420", Main.highestCombination(numberList));
 
     }
 
