99461ce78302571a286b55eae44fa41b6b0810d1
Zason Founder <founder.zason@gmail.com>
2018-01-21 21:14:05 +0200

    Parameterized Test

--- src/com/company/MainTest.java
+++ src/com/company/MainTest.java
@@ -1,20 +1,50 @@
 package com.company;
 
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
 
-class MainTest {
-    @org.junit.jupiter.api.Test
-    void main() {
+
+@RunWith(Parameterized.class)
+public class MainTest {
+
+    @org.junit.Test
+    public void main() {
     }
 
-    @org.junit.jupiter.api.Test
-    void highestCombination() {
-        List<Integer> numberList = new ArrayList<>(List.of(420,42, 423));
+    List<Integer> numberList;
 
-        assertEquals("42423420", Main.highestCombination(numberList));
+    String expected;
+
+    public MainTest (List<Integer> numberList, String expected) {
+
+        this.numberList = new ArrayList<>(numberList);
+        this.expected = expected;
+
+    }
+
+    @org.junit.Test
+    public void highestCombination() {
+
+        assertEquals(expected, Main.highestCombination(numberList));
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> testConditions() {
+        return Arrays.asList(new Object[][]{
+                {Arrays.asList(42, 420, 423),  "42423420"},
+                {Arrays.asList(56, 52, 9), "95652"},
+                {Arrays.asList(5, 50, 56), "56550"},
+                {Arrays.asList(50, 2, 1, 9), "95021"},
+                {Arrays.asList(10, 49, 5690, 8, 1342), "8569049134210"},
+                {Arrays.asList(34, 1, 98, 83), "9883341"}
+        });
 
     }
 
